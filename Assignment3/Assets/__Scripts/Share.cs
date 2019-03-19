3faefc223d4c0cc2bf0cde64901428ab226d9ade
wdai49 <46665590+wdai49@users.noreply.github.com>
2019-03-19 09:41:55 -0400

    fixed repo structure

---
+++ Assignment3/Assets/__Scripts/Share.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Share : MonoBehaviour
+{
+    private const string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
+
+    public void ShareOnTwitter()
+    {
+        string _tweet = "Score: " + ScoreManager.SCORE;
+        Application.OpenURL(TWITTER_ADDRESS + "?text=" + WWW.EscapeURL(_tweet));
+    }
+
+}
